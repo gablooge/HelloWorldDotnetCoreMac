df6c2556fe12d65a436f7f9baddf9130625760b2
gablooge <gablooge@gmail.com>
2020-12-30 16:53:35 +0700

    Hello World Mac!

---
+++ HelloWorld/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloWorld
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        public string Xamarin(string name)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, welcome to Visual Studio for Mac");
+        }
+    }
+}
